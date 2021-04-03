1ee9b229ed55510f20702f290248a236f2ac2613
swill <swilltec@gmail.com>
2021-04-03 07:14:01 -0400

    professor screen

--- functions.php
+++ functions.php
@@ -1,47 +1,61 @@
 
-<?php 
+<?php
 
-    function university_files(){
-        
-        wp_enqueue_style('google_fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
-        wp_enqueue_style('font_awesome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+function university_files()
+{
 
-        if (strstr($_SERVER['SERVER_NAME'], 'localhost')){
-            wp_enqueue_script('university_main_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
-        } else {
-            wp_enqueue_script('university_vendors_js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-            wp_enqueue_script('university_bundled_js', get_theme_file_uri('/bundled-assets/scripts.7c107d9b1f23736f45a8.js'), NULL, '1.0', true);
-            wp_enqueue_style('university_main_styles', get_theme_file_uri('/bundled-assets/styles.7c107d9b1f23736f45a8.css'));
-        }
+    wp_enqueue_style('google_fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+    wp_enqueue_style('font_awesome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
+    if (strstr($_SERVER['SERVER_NAME'], 'localhost')) {
+        wp_enqueue_script('university_main_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('university_vendors_js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('university_bundled_js', get_theme_file_uri('/bundled-assets/scripts.7c107d9b1f23736f45a8.js'), NULL, '1.0', true);
+        wp_enqueue_style('university_main_styles', get_theme_file_uri('/bundled-assets/styles.7c107d9b1f23736f45a8.css'));
+    }
+};
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+
+function university_features()
+{
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
+    register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'university_features');
+
+
+function university_queries($query)
+{
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('postorder', 'ASC');
+        $query->set('posts_per_page', -1);
     }
 
-    add_action('wp_enqueue_scripts', 'university_files');
+    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
 
-    function university_features(){
-        register_nav_menu('headerMenuLocation', 'Header Menu Location');
-        register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
-        register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
-        add_theme_support('title-tag');
-    }
-    add_action('after_setup_theme', 'university_features');
-    function university_queries($query){
-        if(!is_admin() AND is_post_type_archive('event') AND $query -> is_main_query()){
-            $today = date('Ymd');
-            $query->set('meta_key', 'event_date');
-            $query->set('orderby', 'meta_value_num');
-            $query->set('order', 'ASC');
-            $query->set('meta_query', array(
-                array(
-                    'key' => 'event_date',
-                    'compare' => '>=',
-                    'value' => $today,
-                    'type' => 'numeric'
-                )
-                ));
-            
-        }
-    }
-    
-    add_action('pre_get_posts', 'university_queries');
+add_action('pre_get_posts', 'university_queries');
 
 ?>
--- single-professor.php
+++ single-professor.php
@@ -16,35 +16,32 @@ while (have_posts()) {
 
 	<div class="container container--narrow page-section">
 		<div class="generic-content">
-
-			<div class="metabox metabox--position-up metabox--with-home-link">
-				<p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
-						<i class="fa fa-home" aria-hidden="true"></i>
-						Event Home
-					</a> <span class="metabox__main">
-						<?php the_title(); ?>
-					</span></p>
-
+			<div class="row group">
+				<div class="one-third">
+					<?php the_post_thumbnail(); ?>
+				</div>
+				<div class="two-thirds">
+				<?php
+					the_content(); ?>
+				</div>
 			</div>
-
-			<?php the_content(); ?>
 		</div>
 		<?php
 		$relatedPrograms = get_field('related_programs');
-		if ($relatedPrograms){
+		if ($relatedPrograms) {
 
 			echo '<hr class="section-break">';
-			echo '<h2 class="headline headline--medium"> Related Program(s) </h2>';
+			echo '<h2 class="headline headline--medium"> Subject(s) Taught</h2>';
 			echo '<ul class="link-list min-list">';
 			foreach ($relatedPrograms as $program) { ?>
 				<li><a href="<?php echo get_the_permalink($program); ?>">
 						<?php echo get_the_title($program); ?>
 					</a></li>
-			<?php }
-	
+		<?php }
+
 			echo '</ul>';
 		}
-			?>
+		?>
 
 	</div>
 
